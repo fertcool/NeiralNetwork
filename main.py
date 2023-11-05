975351da088186dfb79eab995f767b879f81aac0
артём иващенко <fertcool@gmail.com>
2023-11-06 00:10:32 +0300

    удалена старая модель

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 from openpyxl import load_workbook
 import itertools
 import os
+from tensorflow import keras
 from keras.models import Sequential
 from keras.layers import Dense
 import numpy as np
@@ -32,52 +33,6 @@ class Data:
     ch_3:list[float]
     ch_2:list[float]
     ch_1:list[float]
-
-def functoarray(arr, func):
-    for i in range(len(arr)):
-        arr[i] = func(arr[i])
-
-    return arr
-
-
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-
-
-def sigmoid_derivative(x):
-    return x*(1-x)
-
-
-def Loss(y, y_exp):
-    loss = 0
-    for i in range(len(y)):
-        loss += (y[i]-y_exp[i])**2
-
-    return loss
-
-
-class NeuralNetwork:
-    def __init__(self, x, y):
-        self.input = x
-        self.weights1 = np.random.rand(self.input.shape[1], 70)
-        self.weights2 = np.random.rand(70, 1)
-        self.y = y
-        self.output = np.zeros(self.y.shape)
-
-    def feedforward(self):
-        self.layer1 = sigmoid(np.dot(self.input, self.weights1))
-        self.output = np.dot(self.layer1, self.weights2)
-
-    def backprop(self):
-        # application of the chain rule to find derivative of the loss function with respect to weights2 and weights1
-
-        d_weights2 = np.dot(self.layer1.T, (2 * (self.y - self.output) * sigmoid_derivative(self.output)))
-        d_weights1 = np.dot(self.input.T, (np.dot(2 * (self.y - self.output) * sigmoid_derivative(self.output),
-                                                  self.weights2.T) * sigmoid_derivative(self.layer1)))
-
-        # update the weights with the derivative (slope) of the loss function
-        self.weights1 += d_weights1
-        self.weights2 += d_weights2
 
 
 # Load in the workbook
@@ -115,14 +70,6 @@ for i in range(count):
 InputArr = np.array(InputArr, dtype=float)
 OutputArr = np.array(OutputArr, dtype=float)
 
-CurModel = NeuralNetwork(InputArr, OutputArr)
-CurModel.feedforward()
-CurModel.backprop()
-
-for i in range(1, count*200):
-    CurModel.feedforward()
-    CurModel.backprop()
-    print(Loss(CurModel.output, CurModel.y))
 
 model = Sequential()
 model.add(Dense(count*10, input_dim=16, activation='relu'))
@@ -140,4 +87,7 @@ print(f"Точность модели: {accuracy * 100:.2f}%")
 
 predictions = model.predict(InputArr)
 print(predictions*max(data.humus))
-model.save('16_model')
+# model.save('16_model')
+# model_loaded = keras.models.load_model('16_model')
+# predictions = model_loaded.predict(InputArr[1:6])
+# print(predictions*max(data.humus))
