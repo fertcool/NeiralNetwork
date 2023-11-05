a679cd854a3c2e6c6e066c6f2214cf2155f739e8
артём иващенко <fertcool@gmail.com>
2023-11-05 23:46:00 +0300

    работает погрешность абс = 0.002

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+import random
+
 from openpyxl import load_workbook
 import itertools
 import os
 from keras.models import Sequential
 from keras.layers import Dense
 import numpy as np
-# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+from tensorflow.python.client import device_lib
+
 import tensorflow as tf
-tf.config.list_physical_devices('GPU')
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
 class Data:
@@ -17,6 +20,18 @@ class Data:
     PH_salt: list[float]
     humus: list[float]
 
+    red:list[float]
+    green:list[float]
+    blue:list[float]
+
+    ch_8:list[float]
+    ch_7:list[float]
+    ch_6:list[float]
+    ch_5:list[float]
+    ch_4:list[float]
+    ch_3:list[float]
+    ch_2:list[float]
+    ch_1:list[float]
 
 def functoarray(arr, func):
     for i in range(len(arr)):
@@ -76,36 +91,53 @@ data.hydrolytic_acid = [sheet[f"E{x}"].value for x in range(2, 2 + count)]
 data.PH_water = [sheet[f"F{x}"].value for x in range(2, 2 + count)]
 data.humus = [sheet[f"G{x}"].value for x in range(2, 2 + count)]
 data.PH_salt = [sheet[f"H{x}"].value for x in range(2, 2 + count)]
+data.red = [sheet[f"J{x}"].value for x in range(2, 2 + count)]
+data.green = [sheet[f"K{x}"].value for x in range(2, 2 + count)]
+data.blue = [sheet[f"L{x}"].value for x in range(2, 2 + count)]
+data.ch_8 = [sheet[f"M{x}"].value for x in range(2, 2 + count)]
+data.ch_7 = [sheet[f"N{x}"].value for x in range(2, 2 + count)]
+data.ch_6 = [sheet[f"O{x}"].value for x in range(2, 2 + count)]
+data.ch_5 = [sheet[f"P{x}"].value for x in range(2, 2 + count)]
+data.ch_4 = [sheet[f"Q{x}"].value for x in range(2, 2 + count)]
+data.ch_3 = [sheet[f"R{x}"].value for x in range(2, 2 + count)]
+data.ch_2 = [sheet[f"S{x}"].value for x in range(2, 2 + count)]
+data.ch_1 = [sheet[f"T{x}"].value for x in range(2, 2 + count)]
 
 InputArr = []
 OutputArr = []
 for i in range(count):
-    InputArr.append([data.P205[i], data.K20[i], data.PH_salt[i], data.PH_water[i], data.hydrolytic_acid[i]])
+    InputArr.append([data.P205[i], data.K20[i], data.PH_salt[i], data.PH_water[i], data.hydrolytic_acid[i],
+                     data.red[i], data.green[i], data.blue[i], data.ch_8[i], data.ch_7[i], data.ch_6[i],
+                     data.ch_5[i], data.ch_4[i], data.ch_3[i], data.ch_2[i], data.ch_1[i]])
     OutputArr.append([data.humus[i]])
+    OutputArr[i][0] /= max(data.humus)
 
 InputArr = np.array(InputArr, dtype=float)
 OutputArr = np.array(OutputArr, dtype=float)
-#
-# CurModel = NeuralNetwork(InputArr, OutputArr)
-# CurModel.feedforward()
-# CurModel.backprop()
-#
-# for i in range(1, count*200):
-#     CurModel.feedforward()
-#     CurModel.backprop()
-#     print(Loss(CurModel.output, CurModel.y))
+
+CurModel = NeuralNetwork(InputArr, OutputArr)
+CurModel.feedforward()
+CurModel.backprop()
+
+for i in range(1, count*200):
+    CurModel.feedforward()
+    CurModel.backprop()
+    print(Loss(CurModel.output, CurModel.y))
 
 model = Sequential()
-model.add(Dense(count, input_dim=5, activation='relu'))
-model.add(Dense(count, activation='relu'))
-
-
+model.add(Dense(count*10, input_dim=16, activation='relu'))
+model.add(Dense(count*10, activation='relu'))
+model.add(Dense(count*20, activation='relu'))
+model.add(Dense(count*20, activation='relu'))
+model.add(Dense(count*30, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(InputArr, OutputArr, epochs=500)
+
+model.compile(loss='mae', optimizer='adam', metrics=['accuracy'])
+model.fit(InputArr, OutputArr, epochs=10000)
 
 loss, accuracy = model.evaluate(InputArr, OutputArr)
 print(f"Точность модели: {accuracy * 100:.2f}%")
 
 predictions = model.predict(InputArr)
-print(predictions)
+print(predictions*max(data.humus))
+model.save('16_model')
